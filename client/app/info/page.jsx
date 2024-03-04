480c3c7bd0be8f2ddda6177d10a852c4d65a56bf
Ilia Loginov <i.loginow.by@gmail.com>
2024-03-04 13:47:25 +0100

    fix: data description

--- client/app/info/page.jsx
+++ client/app/info/page.jsx
@@ -5,8 +5,11 @@ export default function Info() {
   const intro = 'The data set includes data from a direct marketer who sells his products only via direct mail. He sends catalogs with product characteristics to customers who then order directly from the catalogs. The marketer has developed customer records to learn what makes some customers spend more than others.';
   const dataSetInfo = 'The data set includes n = 1000 customers and the following variables:';
   const variablesInfo = [
-    'Age (of customer; old/middle/young); Gender (male/female); OwnHome (whether customer owns home; yes/no);',
-    'Married (single/married); Location (far/close; in terms of distance to the nearest brick and mortar store that sells similar products);',
+    'Age (of customer; old/middle/young);',
+    'Gender (male/female);',
+    'OwnHome (whether customer owns home; yes/no);',
+    'Married (single/married);',
+    'Location (far/close; in terms of distance to the nearest brick and mortar store that sells similar products);',
     'Salary (yearly salary of customer; in dollars);',
     'Children (number of children; 0–3);',
     'History (of previous purchase volume; low/medium/high/NA; NA means that this customer has not yet purchased);',
